0f0176d29d26a8a2e2d95001b3ca070b8a4017a5
Mike Doan <manhdoanmainmail@gmail.com>
2023-12-03 16:28:17 -0500

    simplified day 3 code a bit more

--- 2023/day3.py
+++ 2023/day3.py
@@ -2,25 +2,21 @@ filename='inputday3.txt'
 
 def find_numbers(s, index):
     results=[]
-    size=10
-    while size > 0:
+    for size in range(10,0, -1):
         for i in range(len(s)-size+1):
             substring= s[i:i+size]
             if substring.isdigit():
                 results.append((int(substring), i, i+size-1, index))
                 s = s.replace(substring, '.'*size, 1)
-        size -= 1
     return results
 
 def is_adjacent(lines, number):
     def check_line(line, number):
         left = 1 if number[1] != 0 else 0
         right = 1 if number[2] != len(line) else 0
-        check_area=line[number[1]-left : number[2]+right+1]
-        for _ in check_area:
-            if _ in '*#+$/&%-@=':
-                return True
-        return False 
+        check_area=set( line[number[1]-left : number[2]+right+1] )
+        return check_area & set('*#+$/&%-@=')
+
     result = False 
     result = True if check_line(lines[number[3]], number) else result 
     result = True if number[3] > 0 and check_line(lines[number[3]-1], number) else result
@@ -29,27 +25,23 @@ def is_adjacent(lines, number):
 
 def find_gears(s, index):
     results=[]
-    size=1
-    while size > 0:
-        for i in range(len(s)-size+1):
-            substring= s[i:i+size]
-            if substring == "*":
-                results.append((substring, i, i+size-1, index))
-                s = s.replace(substring, '.'*size, 1)
-        size -= 1
+    for i in range(len(s)-2):
+        substring= s[i:i+1]
+        if substring == "*":
+            results.append((substring, i, i, index))
+            s = s.replace(substring, '.', 1)
     return results
 
-def get_coords(number):
-    return set([(x, number[3]) for x in range(number[1], number[2]+1)])
-
 def gear_adjacent(gear, numbers):
+    def get_coords(number):
+        return set([(x, number[3]) for x in range(number[1], number[2]+1)])
     count = 0
     adjacent=[]
     potential = set([ 
         (gear[1]-1, gear[3]-1), (gear[1], gear[3]-1), (gear[1]+1, gear[3]-1),
         (gear[1]-1, gear[3]),   (gear[1], gear[3]),   (gear[1]+1, gear[3]),
         (gear[1]-1, gear[3]+1), (gear[1], gear[3]+1), (gear[1]+1, gear[3]+1)])
-    for i, number in enumerate(numbers):
+    for number in numbers:
         if get_coords(number) & potential:
             count += 1
             adjacent.append(number)
@@ -65,8 +57,7 @@ with open(filename, 'r') as file:
         lines.append(line.strip())
 
 for _ in numbers:
-    if is_adjacent(lines, _):
-        total+= _[0]
+    total = total + _[0] if is_adjacent(lines, _) else total
 
 print(f"Part 1: {total}")
 
@@ -80,5 +71,3 @@ for _ in gears:
         prod += curr[0][0] * curr[1][0]
 
 print(f"Part 2: {prod}") 
-
-
