fab33efc69d086f682f42f5456af6c0f2634a5c8
Mike Doan <manhdoanmainmail@gmail.com>
2023-12-07 12:48:14 -0500

    refactor on day 7

--- 2023/day7.py
+++ 2023/day7.py
@@ -1,80 +1,58 @@
 from collections import Counter
 
+def match_hand(hand,cards,line):
+    match cards:
+        case 1: return '6' + line 
+        case 2:
+            for _ in hand: 
+                if hand.count(_) == 4:
+                    return '5' + line
+            return '4' + line
+        case 3:
+            for _ in hand:
+                if hand.count(_) == 3:
+                    return '3' + line
+            return '2' + line
+        case 4: return '1' + line
+    return '0' + line 
+
 def parse_hand(line):
     translation_table = str.maketrans('AKQJT98765432', 'MLKJIHGFEDCBA')
     line = line.split()[0].translate(translation_table) + " " + line.split()[1]
     hand = line.split()[0] # order = A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2
     cards = len(set(hand)) # order = M  L  K  J  I  H  G  F  E  D  C  B  A 
-    if cards == 1:
-        return '6' + line 
-    if cards == 2:
-        for _ in hand: 
-            if hand.count(_) == 4:
-                return '5' + line
-        return '4' + line
-    if cards == 3:
-        for _ in hand:
-            if hand.count(_) == 3:
-                return '3' + line
-        return '2' + line
-    if cards == 4:
-        return '1' + line
-    return '0' + line 
-
+    return match_hand(hand , cards, line)
+    
 def parse_hand2(line):
-
-    def changeJ(hand):
-        top = Counter(hand).most_common()
-        if len(top) == 1: return hand
-        hand = hand.replace('A', top[0][0]) if top[0][0] != 'A' else hand.replace('A', top[1][0])
-        return hand
-
     translation_table = str.maketrans('AKQT98765432J', 'MLKJIHGFEDCBA')
     line = line.split()[0].translate(translation_table) + " " + line.split()[1]
     hand = line.split()[0] # order = A, K, Q, T, 9, 8, 7, 6, 5, 4, 3, 2, J
     cards = len(set(hand)) # order = M  L  K  J  I  H  G  F  E  D  C  B  A 
     if 'A' in hand:
-        cards = cards - 1 
-        hand = changeJ(hand)
-        cards = 1 if cards == 0 else cards
+            cards = cards - 1 
+            top = Counter(hand).most_common()
+            if len(top) != 1: 
+                hand = hand.replace('A', top[0][0]) if top[0][0] != 'A' else hand.replace('A', top[1][0])
+            cards = 1 if cards == 0 else cards
+    return match_hand(hand, cards, line)
 
-    if cards == 1:
-        return '6' + line 
-    if cards == 2:
-        for _ in hand: 
-            if hand.count(_) == 4:
-                return '5' + line
-        return '4' + line
-    if cards == 3:
-        for _ in hand:
-            if hand.count(_) == 3:
-                return '3' + line
-        return '2' + line
-    if cards == 4:
-        return '1' + line
-    return '0' + line 
+if __name__ == "__main__":
+    lines = open("./inputday7.txt").readlines()
+    hands, hands2 = [], []
+    p1, p2 = 0, 0
+    for line in lines:
+        hands.append(parse_hand(line.strip()))
+        hands2.append(parse_hand2(line.strip()))
+    hands.sort()
+    hands2.sort()
 
+    for i, _ in enumerate(hands):
+        p1 += int(_.split()[1])*(i+1)
+    print(f"Part 1: {p1}")
 
-lines = open("./inputday7.txt").readlines()
-hands = []
-p1 = 0
-for line in lines:
-    hands.append(parse_hand(line.strip()))
-hands.sort()
-for i, _ in enumerate(hands):
-    p1 += int(_.split()[1])*(i+1)
-
-print(f"Part 1: {p1}")
-
-hands2 = []
-p2 = 0
-for line in lines:
-    hands2.append(parse_hand2(line.strip()))
-hands2.sort()
-for i, _ in enumerate(hands2):
-    p2 += int(_.split()[1])*(i+1)
-
-print(f"Part 2: {p2}")
+    for i, _ in enumerate(hands2):
+        p2 += int(_.split()[1])*(i+1)
+    print(f"Part 2: {p2}")
 
 
 
