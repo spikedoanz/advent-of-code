5a41a5897c5dd16b109fe0eb7fdb6259674ac7b1
Mike Doan <manhdoanmainmail@gmail.com>
2024-04-08 21:05:47 -0400

    2020/13/rust, really fun problem, should've spent more time thinking about it

--- 2020/day13.rs
+++ 2020/day13.rs
@@ -1,5 +1,4 @@
 mod aoc; use aoc::*;
-use std::collections::HashSet;
 
 fn part1(filename: &str) -> i32 {
     let mut lines: Vec<String> = Vec::new();
@@ -26,9 +25,18 @@ fn part1(filename: &str) -> i32 {
     return min.ceil() as i32 * minbus;
 }
 
-/*
-// Way too slow :(((
-fn part2(filename: &str) -> i32 {
+
+fn shiftedlcm(a: i64, b: i64, a_s: i64, b_s: i64, shift: i64) -> i64 {
+    let mut n = 1;
+    loop {
+        if (a - b + shift + a_s * n ) % b_s == 0 {
+            return a + a_s * n;
+        }
+        n += 1;
+    }
+}
+
+fn part2(filename: &str) -> i64 {
     let mut lines: Vec<String> = Vec::new();
     for line in readlines(filename).expect("lines") {
         lines.push(line.expect("line"));
@@ -39,89 +47,26 @@ fn part2(filename: &str) -> i32 {
     let mut buses: Vec<i64> = Vec::new();
     for bus in sched {
         match bus {
-            "x" => idx -= 1,
+            "x" => idx += 1,
             _ => {
                 shift.push(idx);
                 buses.push(bus.parse::<i64>().unwrap());
-                idx -=1;
+                idx +=1;
             }
         }
     }
-    println!("{:?}", shift);
-    println!("{:?}", buses);
-    let mut sievesz: i64 = 1;
-    for bus in &buses[6..] {
-        sievesz *= bus;
+    let mut range = buses[0];
+    let mut ret: i64 = 0;
+    while buses.len() > 1 {
+        shift.remove(0);
+        ret = shiftedlcm(buses[0], buses[1], range, buses[1], shift[0]);
+        range *= buses[1];
+        buses.remove(0);
+        buses[0] = ret;
     }
-    //let mut currs: i64 = 100001568000000;
-    let mut currs: i64 = 0;
-    let mut num: i64 = 0;
-    let mut sieves: Vec<HashSet<i64>> = Vec::new();
-    let mut sievea: HashSet<i64> = (0..sievesz).collect(); 
-    for (s, b) in shift.iter().zip(buses.iter()) {
-        let mut sieveb = HashSet::new();
-        for i in 0..(sievesz/b+1) {
-            num = i*b+s; 
-            sieveb.insert(num);
-        }
-        sieves.push(sievea.intersection(&sieveb).cloned().collect::<HashSet<_>>());
-    }
-
-    /*
-    println!("{:?}", sievea);
-    for sieve in &sieves {
-        println!("{:?}", sieve);
-    }
-    */
-    for _ in 0..100000000 as i64 { 
-        let mut sievea = sieves[0].clone();
-
-        for sieve in &sieves[1..] {
-            sievea = sievea.intersection(sieve).cloned().collect::<HashSet<_>>();
-        }
-
-        if !sievea.is_empty() {
-            println!("{:?}", sievea);
-            return 1;
-        }
-        
-        for sieve in &mut sieves {
-            *sieve = sieve.iter().map(|&x| x + sievesz).collect();
-        }
-        currs += sievesz;
-        println!("{}", currs);
-    }
-
-    return 0;
+    return ret;
 }
-*/
 
-fn part2(filename: &str) -> i32 {
-    let mut lines: Vec<String> = Vec::new();
-    for line in readlines(filename).expect("lines") {
-        lines.push(line.expect("line"));
-    }
-    let sched = lines[1].split(",");
-    let mut idx = 0;
-    let mut shift: Vec<i64> = Vec::new();
-    let mut buses: Vec<i64> = Vec::new();
-    for bus in sched {
-        match bus {
-            "x" => idx -= 1,
-            _ => {
-                shift.push(idx);
-                buses.push(bus.parse::<i64>().unwrap());
-                idx -=1;
-            }
-        }
-    }
-    println!("{:?}", shift);
-    println!("{:?}", buses);
-    let N: i64 = buses.iter().copied().reduce(|a, b| a*b).unwrap();
-    println!("{}", N);
-
-    return 0;
-}
 fn main() {
     println!("Part 1: {}", part1("i13.txt"));
     println!("Part 2: {}", part2("i13.txt"));
