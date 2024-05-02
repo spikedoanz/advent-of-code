f2b5d867c1541500f438f5f3633553ca1699022e
Mike Doan <manhdoanmainmail@gmail.com>
2024-05-02 00:54:34 -0400

    2020/18/1/rust

---
+++ 2020/day18.rs
@@ -0,0 +1,54 @@
+mod aoc; use aoc::*;
+
+fn apply(a: i64 , b: i64, op: i64) -> i64 {
+    if op == 0 { return a + b; }
+    return a * b;
+}
+
+fn parse(line: &[char], i: &mut usize) -> i64 {
+    let mut ret = 0;
+    let mut op = 0; // 0 means +, 1 means *
+    while *i < line.len() {
+        let c = line[*i];
+        match c {
+            '0'..='9' => {
+                ret = apply(ret, c as i64 - '0' as i64, op);
+            },
+            '*' => { op = 1 },
+            '+' => { op = 0 },
+            '(' => { 
+                *i += 1;
+                ret = apply(ret, parse(line, i), op);
+            },
+            ')' => {
+                return ret;
+            }
+            _ => {},
+        }
+        *i += 1;
+    }
+    return ret;
+}
+
+fn part1(filename: &str) -> i64 {
+    let mut ret: i64 = 0;
+    let mut lines: Vec<String> = Vec::new();
+    for line in readlines(filename).expect("lines") {
+        lines.push(line.expect("line"));
+    }
+    for line in lines {
+        let mut i: usize = 0;
+        println!("{}", line);
+        // println!(" -> {}", parse(&line.chars().collect::<Vec<_>>(), &mut i));
+        ret += parse(&line.chars().collect::<Vec<_>>(), &mut i);
+        // println!("{}", u);
+    }
+
+    return ret;
+}
+
+
+fn main() {
+  println!("Part 1: {}", part1("i18.txt"));
+  // println!("Part 2: {}", part2("i17.txt"));
+}
