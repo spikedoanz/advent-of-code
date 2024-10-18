2aa951a81cd0f04fc0e03f1eea1d1ba86c03b52d
Mike Doan <spikedoanz@gmail.com>
2024-10-18 17:48:12 -0400

    halting problem lmfao

--- 2018/day1.lean
+++ 2018/day1.lean
@@ -1,8 +1,8 @@
-import Lean.Data.HashMap
+import Lean.Data.HashSet
 
 def getInt (s: String) : Option Int :=
   if s.isEmpty then
-    some 0
+    none
   else if s.front == '-' then
     s.toInt?
   else
@@ -16,11 +16,30 @@ def part1 (lines : List String) : Int :=
     | some n => n + part1 rest
     | none => part1 rest
 
-def main : IO Unit := do
-  let text ← IO.FS.readFile "2018/example.txt"
-  IO.println "Part 1:"
-  IO.println (part1 (text.splitOn "\n"))
+def part2 (curr : Int := 0)
+          (lines : List String)
+          (seen : Lean.HashSet Int := Lean.HashSet.empty) 
+          (source : List String) 
+          (max_loops : Nat := 10^20) : Int :=
+  if max_loops = 0 then
+    0
+  else
+    match lines with
+    | [] => part2 curr source seen source (max_loops - 1)
+    | line :: rest =>
+      if seen.contains curr then
+        curr
+      else
+        match getInt line with
+        | some n => part2 (curr + n) rest (seen.insert curr) source (max_loops - 1)
+        | none => part2 curr rest seen source (max_loops - 1)
+termination_by max_loops
 
+def main : IO Unit := do
+  let text ← IO.FS.readFile "2018/inputday1.txt"
+  IO.println "-- Part 1:"
+  IO.println (part1 (text.splitOn "\n"))
+  IO.println "-- Part 2:"
+  IO.println (part2 0 (lines := text.splitOn "\n") (source := text.splitOn "\n"))
 
 #eval main
-
